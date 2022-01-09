c076e1dd7f06fc07e459d05f0759edf011d562f3
asdqwerty <theasdqwerty@gmail.com>
2022-01-10 02:09:59 +0300

    Проверка отмены.

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace ConsoleTest
+﻿using System;
+
+namespace ConsoleTest
 {
     internal static class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Say hello");
         }
     }
 }
